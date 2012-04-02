723fec29ff7fd5aed524bf665f3fb6d4b269c4cd
Lindsay Evans <linz@linz.id.au>
2012-04-02 19:32:49 +1000

    Better check for standalone image

--- src/background.js
+++ src/background.js
@@ -1,14 +1,7 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-// Called when a message is passed.  We assume that the content script
-// wants to show the page action.
 
 var active = false;
 
 function onRequest(request, sender, sendResponse) {
-  // Show the page action for the tab that the sender (content script)
-  // was on.
 	chrome.pageAction.show(sender.tab.id);
 
 	chrome.pageAction.onClicked.addListener(function(tab){
@@ -19,10 +12,9 @@ function onRequest(request, sender, sendResponse) {
 		});
   });
 
-
-
   // Return nothing to let the connection be cleaned up.
   sendResponse({});
 };
+
 // Listen for the content script to send a message to the background page.
 chrome.extension.onRequest.addListener(onRequest);
--- src/contentscript.js
+++ src/contentscript.js
@@ -1,21 +1,17 @@
-/*
- * Copyright (c) 2010 The Chromium Authors. All rights reserved.  Use of this
- * source code is governed by a BSD-style license that can be found in the
- * LICENSE file.
- */
-var regex = /\.(png|gif)$/;
-// Test the text of the body element against our regular expression.
+
 // TODO:
-// - better checking of whether we're viewing an image standalone
 // - check if the image has transparency? (get image pixel data, look through til trans/semi-trans found)
-if (regex.test(location.pathname)) {
-  // The regular expression produced a match, so notify the background page.
+
+
+// This is currently the best check for whether we're viewing an image by itself
+var standalone_images = document.querySelectorAll('img[style*="-webkit-user-select: none; cursor: -webkit-zoom"]');
+if(standalone_images.length === 1){
+	// Send message to background page - displays the page action icon 
   chrome.extension.sendRequest({}, function (response) {});
-} else {
-  // No match was found.
 }
 
+// Listen for request from the background page - toggle 'active' state of page action icon
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-	document.getElementsByTagName('IMG')[0].style.background = request.active ? 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAAA3NCSVQICAjb4U/gAAAABlBMVEX////f39/D27fmAAAACXBIWXMAAAsSAAALEgHS3X78AAAAHHRFWHRTb2Z0d2FyZQBBZG9iZSBGaXJld29ya3MgQ1M1cbXjNgAAABZ0RVh0Q3JlYXRpb24gVGltZQAwMy8zMC8xMp+A10AAAAAQSURBVAiZY2D4z4AV4RAGAH6/D/F9RRmSAAAAAElFTkSuQmCC)' : '';
+	standalone_images[0].style.background = request.active ? 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAAA3NCSVQICAjb4U/gAAAABlBMVEX////f39/D27fmAAAACXBIWXMAAAsSAAALEgHS3X78AAAAHHRFWHRTb2Z0d2FyZQBBZG9iZSBGaXJld29ya3MgQ1M1cbXjNgAAABZ0RVh0Q3JlYXRpb24gVGltZQAwMy8zMC8xMp+A10AAAAAQSURBVAiZY2D4z4AV4RAGAH6/D/F9RRmSAAAAAElFTkSuQmCC)' : '';
 });
 
