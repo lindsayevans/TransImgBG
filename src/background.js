515bf59283721d828b3e1094876305342347f1d3
Lindsay Evans <linz@linz.id.au>
2012-04-02 22:21:38 +1000

    Adding initial context menu bits

--- src/background.js
+++ src/background.js
@@ -1,5 +1,6 @@
 
-var active = false;
+var active = false,
+    ctx_menu_id;
 
 function onRequest(request, sender, sendResponse) {
 	chrome.pageAction.show(sender.tab.id);
@@ -9,6 +10,7 @@ function onRequest(request, sender, sendResponse) {
 		chrome.pageAction.setIcon({tabId: tab.id, path: active ? 'icon.png' : 'icon_inactive.png'});
 		chrome.pageAction.setTitle({tabId: tab.id, title: active ? 'Hide transparency' : 'Show transparency'});
 		chrome.tabs.sendRequest(tab.id, {active: active}, function(response) {
+			chrome.contextMenus.update(ctx_menu_id, {checked: active});
 		});
   });
 
@@ -17,6 +19,7 @@ function onRequest(request, sender, sendResponse) {
 		chrome.pageAction.setIcon({tabId: sender.tab.id, path: active ? 'icon.png' : 'icon_inactive.png'});
 		chrome.pageAction.setTitle({tabId: sender.tab.id, title: active ? 'Hide transparency' : 'Show transparency'});
 		chrome.tabs.sendRequest(sender.tab.id, {active: active}, function(response) {
+			chrome.contextMenus.update(ctx_menu_id, {checked: active});
 		});
 	}
 
@@ -26,3 +29,20 @@ function onRequest(request, sender, sendResponse) {
 
 // Listen for the content script to send a message to the background page.
 chrome.extension.onRequest.addListener(onRequest);
+
+// TODO:
+// - Make checked work with saved options
+// - Make it appear top level of menu
+ctx_menu_id = chrome.contextMenus.create({
+	type: 'checkbox',
+	title: 'Show image transparency',
+	checked: active,
+	contexts: ['image'],
+	onclick: function(info, tab){
+		active = !active;
+		chrome.pageAction.setIcon({tabId: tab.id, path: active ? 'icon.png' : 'icon_inactive.png'});
+		chrome.pageAction.setTitle({tabId: tab.id, title: active ? 'Hide transparency' : 'Show transparency'});
+		chrome.tabs.sendRequest(tab.id, {active: active}, function(response) {
+		});
+	}
+});
--- src/contentscript.js
+++ src/contentscript.js
@@ -2,7 +2,6 @@
 // TODO:
 // - check if the image has transparency? (get image pixel data, look through til trans/semi-trans found)
 // - moar options (background colour)
-// - context menu item?
 
 
 // This is currently the best check for whether we're viewing an image by itself
