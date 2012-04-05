a20182ffa19415fdceecff97a260201069dd6780
Lindsay Evans <linz@linz.id.au>
2012-04-05 14:03:45 +1000

    Misc tweaks; getting bits for Chrome Web Store

--- src/background.js
+++ src/background.js
@@ -11,7 +11,7 @@ function onRequest(request, sender, sendResponse) {
 		background_colour = (localStorage["background-colour"] || '#fff');
 		chrome.pageAction.setIcon({tabId: tab.id, path: active ? 'icon.png' : 'icon_inactive.png'});
 		chrome.pageAction.setTitle({tabId: tab.id, title: active ? 'Hide transparency' : 'Show transparency'});
-		chrome.contextMenus.update(ctx_menu_id, {checked: active});
+		chrome.contextMenus.update(ctx_menu_id, {title: active ? 'Hide image transparency' : 'Show image transparency'});
 		chrome.tabs.sendRequest(tab.id, {active: active, background_colour: background_colour});
   });
 
@@ -20,7 +20,7 @@ function onRequest(request, sender, sendResponse) {
 		background_colour = (localStorage["background-colour"] || '#fff');
 		chrome.pageAction.setIcon({tabId: sender.tab.id, path: active ? 'icon.png' : 'icon_inactive.png'});
 		chrome.pageAction.setTitle({tabId: sender.tab.id, title: active ? 'Hide transparency' : 'Show transparency'});
-		chrome.contextMenus.update(ctx_menu_id, {checked: active});
+		chrome.contextMenus.update(ctx_menu_id, {title: active ? 'Hide image transparency' : 'Show image transparency'});
 		chrome.tabs.sendRequest(sender.tab.id, {active: active, background_colour: background_colour});
 	}
 
@@ -34,15 +34,14 @@ chrome.extension.onRequest.addListener(onRequest);
 // TODO:
 // - Make it appear top level of menu
 ctx_menu_id = chrome.contextMenus.create({
-	type: 'checkbox',
-	title: 'Show image transparency',
-	checked: active,
+	title: active ? 'Hide image transparency' : 'Show image transparency',
 	contexts: ['image'],
 	onclick: function(info, tab){
 		active = !active;
 		background_colour = (localStorage["background-colour"] || '#fff');
 		chrome.pageAction.setIcon({tabId: tab.id, path: active ? 'icon.png' : 'icon_inactive.png'});
 		chrome.pageAction.setTitle({tabId: tab.id, title: active ? 'Hide transparency' : 'Show transparency'});
+		chrome.contextMenus.update(ctx_menu_id, {title: active ? 'Hide image transparency' : 'Show image transparency'});
 		chrome.tabs.sendRequest(tab.id, {active: active, background_colour: background_colour});
 	}
 });
--- src/contentscript.js
+++ src/contentscript.js
@@ -5,7 +5,7 @@
 
 
 // This is currently the best check for whether we're viewing an image by itself
-var standalone_images = document.querySelectorAll('body > img[style*="-webkit-user-select: none; cursor: -webkit-zoom"]');
+var standalone_images = document.querySelectorAll('body > img[style*="-webkit-user-select: none"]');
 if(standalone_images.length === 1){
 	// Send message to background page - displays the page action icon 
   chrome.extension.sendRequest({}, function (response) {});
@@ -13,6 +13,6 @@ if(standalone_images.length === 1){
 
 // Listen for request from the background page - toggle 'active' state of page action icon
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-	standalone_images[0].style.background = request.active ? request.background_colour + ' url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAAA3NCSVQICAjb4U/gAAAABlBMVEX////f39/D27fmAAAACXBIWXMAAAsSAAALEgHS3X78AAAAHHRFWHRTb2Z0d2FyZQBBZG9iZSBGaXJld29ya3MgQ1M1cbXjNgAAABZ0RVh0Q3JlYXRpb24gVGltZQAwMy8zMC8xMp+A10AAAAAQSURBVAiZY2D4z4AV4RAGAH6/D/F9RRmSAAAAAElFTkSuQmCC)' : '';
+	standalone_images[0].style.background = request.active ? request.background_colour + ' url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAABZ0RVh0Q3JlYXRpb24gVGltZQAwMy8zMC8xMp+A10AAAAAcdEVYdFNvZnR3YXJlAEFkb2JlIEZpcmV3b3JrcyBDUzVxteM2AAAAMklEQVQ4jWP8//8/Az7AyMioiE+eCa9uIsCoAYPBAEYGBga88fz////7NHXBqAGDwQAAg64HPX2afs4AAAAASUVORK5CYII=)' : '';
 });
 
