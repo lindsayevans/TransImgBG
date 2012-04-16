9d42d90b196d052db71e496df7a2295bb412ec62
Lindsay Evans <linz@linz.id.au>
2012-04-16 12:04:56 +1000

    Refactoring BG toggle

--- src/background.js
+++ src/background.js
@@ -8,20 +8,12 @@ function onRequest(request, sender, sendResponse) {
 
 	chrome.pageAction.onClicked.addListener(function(tab){
 		active = !active;
-		background_colour = (localStorage["background-colour"] || '#fff');
-		chrome.pageAction.setIcon({tabId: tab.id, path: active ? 'icon.png' : 'icon_inactive.png'});
-		chrome.pageAction.setTitle({tabId: tab.id, title: active ? 'Hide transparency' : 'Show transparency'});
-		chrome.contextMenus.update(ctx_menu_id, {title: active ? 'Hide image transparency' : 'Show image transparency'});
-		chrome.tabs.sendRequest(tab.id, {active: active, background_colour: background_colour});
+		toggle_transparency();
   });
 
 	if(localStorage["show-transparency-default"]){
 		active = localStorage["show-transparency-default"] === 'true';
-		background_colour = (localStorage["background-colour"] || '#fff');
-		chrome.pageAction.setIcon({tabId: sender.tab.id, path: active ? 'icon.png' : 'icon_inactive.png'});
-		chrome.pageAction.setTitle({tabId: sender.tab.id, title: active ? 'Hide transparency' : 'Show transparency'});
-		chrome.contextMenus.update(ctx_menu_id, {title: active ? 'Hide image transparency' : 'Show image transparency'});
-		chrome.tabs.sendRequest(sender.tab.id, {active: active, background_colour: background_colour});
+		toggle_transparency();
 	}
 
   // Return nothing to let the connection be cleaned up.
@@ -31,17 +23,19 @@ function onRequest(request, sender, sendResponse) {
 // Listen for the content script to send a message to the background page.
 chrome.extension.onRequest.addListener(onRequest);
 
-// TODO:
-// - Make it appear top level of menu
 ctx_menu_id = chrome.contextMenus.create({
 	title: active ? 'Hide image transparency' : 'Show image transparency',
 	contexts: ['image'],
 	onclick: function(info, tab){
 		active = !active;
-		background_colour = (localStorage["background-colour"] || '#fff');
-		chrome.pageAction.setIcon({tabId: tab.id, path: active ? 'icon.png' : 'icon_inactive.png'});
-		chrome.pageAction.setTitle({tabId: tab.id, title: active ? 'Hide transparency' : 'Show transparency'});
-		chrome.contextMenus.update(ctx_menu_id, {title: active ? 'Hide image transparency' : 'Show image transparency'});
-		chrome.tabs.sendRequest(tab.id, {active: active, background_colour: background_colour});
+		toggle_transparency();
 	}
 });
+
+function toggle_transparency(){
+	background_colour = (localStorage["background-colour"] || '#fff');
+	chrome.pageAction.setIcon({tabId: tab.id, path: active ? 'icon.png' : 'icon_inactive.png'});
+	chrome.pageAction.setTitle({tabId: tab.id, title: active ? 'Hide transparency' : 'Show transparency'});
+	chrome.contextMenus.update(ctx_menu_id, {title: active ? 'Hide image transparency' : 'Show image transparency'});
+	chrome.tabs.sendRequest(tab.id, {active: active, background_colour: background_colour});
+}
