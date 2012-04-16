34ca63cfe32305913d4c5acb90fbde92300973ea
Lindsay Evans <linz@linz.id.au>
2012-04-16 13:40:37 +1000

    Dicking around with context menus

--- src/background.js
+++ src/background.js
@@ -6,16 +6,26 @@ var active = false,
 function onRequest(request, sender, sendResponse) {
 	chrome.pageAction.show(sender.tab.id);
 
+	if(!ctx_menu_id){
+		ctx_menu_id = chrome.contextMenus.create({
+			title: active ? 'Hide image transparency' : 'Show image transparency',
+			contexts: ['image'],
+			onclick: function(info, tab){
+				active = !active;
+				toggle_transparency(tab);
+			}
+		});
+	}
+
 	chrome.pageAction.onClicked.addListener(function(tab){
 		active = !active;
-		toggle_transparency();
-  });
+		toggle_transparency(tab);
+	});
 
 	if(localStorage["show-transparency-default"]){
 		active = localStorage["show-transparency-default"] === 'true';
-		toggle_transparency();
+		toggle_transparency(sender.tab);
 	}
-
   // Return nothing to let the connection be cleaned up.
   sendResponse({});
 };
@@ -23,16 +33,8 @@ function onRequest(request, sender, sendResponse) {
 // Listen for the content script to send a message to the background page.
 chrome.extension.onRequest.addListener(onRequest);
 
-ctx_menu_id = chrome.contextMenus.create({
-	title: active ? 'Hide image transparency' : 'Show image transparency',
-	contexts: ['image'],
-	onclick: function(info, tab){
-		active = !active;
-		toggle_transparency();
-	}
-});
 
-function toggle_transparency(){
+function toggle_transparency(tab){
 	background_colour = (localStorage["background-colour"] || '#fff');
 	chrome.pageAction.setIcon({tabId: tab.id, path: active ? 'icon.png' : 'icon_inactive.png'});
 	chrome.pageAction.setTitle({tabId: tab.id, title: active ? 'Hide transparency' : 'Show transparency'});
